76a11de9b05f84ad4be64a9a7fbc901adbb37816
Jennifer Rullmann <jen@redmonocle.com>
2013-06-06 10:21:58 -0400

    Added simplepage app

--- geekchic/settings.py
+++ geekchic/settings.py
@@ -132,7 +132,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'workshop',
-    'feedback'
+    'feedback',
+    'simplepage'
 )
 
 # A sample logging configuration. The only tangible logging
