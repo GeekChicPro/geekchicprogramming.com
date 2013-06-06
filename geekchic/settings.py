b085cab4771c56106d65affc55ca53bd666fd821
Jennifer Rullmann <jen@redmonocle.com>
2013-06-06 10:17:59 -0400

    Configured sqlite3 as the default database

--- geekchic/settings.py
+++ geekchic/settings.py
@@ -16,8 +16,8 @@ ROOT_PATH = os.path.join(PROJECT_PATH, "..")
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'geekchic.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
